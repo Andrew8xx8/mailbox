2b6413d5491db38973d0633298f48a5711fb6641
Andreww8xx8 <avk@8xx8.ru>
2014-10-04 04:49:46 +0400

    Backend stubs added

---
+++ src/scripts/repositories/MailRepository.js
@@ -0,0 +1,69 @@
+'use strict';
+
+var _ = require('lodash');
+var Q = require('q');
+
+function backendLatency() {
+  return Math.round(Math.random() * 100);
+}
+
+module.exports = {
+  search: function(options) {
+    var deferred = Q.defer();
+
+    // Эмулируем задержку бекенда
+    setTimeout(function() {
+      var data = require('./data.json');
+
+      if (_.has(options, "token") && options.token.lenth > 3) {
+        data = _.filter(data, function(item) {
+          return item.subjectindexOf(options.token) > -1;
+        });
+      }
+
+      deferred.resolve(data);
+    }, backendLatency());
+
+    return deferred.promise;
+  },
+
+  destroy: function(mail) {
+    var deferred = Q.defer();
+
+    setTimeout(function() {
+      deferred.resolve(mail);
+    }, backendLatency());
+
+    return deferred.promise;
+  },
+
+  markAsRead: function(mail) {
+    var deferred = Q.defer();
+
+    setTimeout(function() {
+      deferred.resolve(mail);
+    }, backendLatency());
+
+    return deferred.promise;
+  },
+
+  markAsUnRead: function(mail) {
+    var deferred = Q.defer();
+
+    setTimeout(function() {
+      deferred.resolve(mail);
+    }, backendLatency());
+
+    return deferred.promise;
+  },
+
+  toggleFavorite: function(mail) {
+    var deferred = Q.defer();
+
+    setTimeout(function() {
+      deferred.resolve(mail);
+    }, backendLatency());
+
+    return deferred.promise;
+  },
+};
